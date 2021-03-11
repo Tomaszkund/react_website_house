7cd52ef3b06b48d71b68f01892528637afd652e2
Tomasz <tomasz.kundys@gmail.com>
2021-03-11 16:31:18 +0100

    Updates

---
+++ precache-manifest.61deae03692e9f679481632308fbf17c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e40be0f9bb98d40388954b083c95c51",
+    "url": "/React_website.git/index.html"
+  },
+  {
+    "revision": "468349726aad89d4e005",
+    "url": "/React_website.git/static/css/main.b211477d.chunk.css"
+  },
+  {
+    "revision": "f126c46f06c2504d94c5",
+    "url": "/React_website.git/static/js/2.4db31091.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React_website.git/static/js/2.4db31091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468349726aad89d4e005",
+    "url": "/React_website.git/static/js/main.192f9c2a.chunk.js"
+  },
+  {
+    "revision": "d890541530200c374d93",
+    "url": "/React_website.git/static/js/runtime-main.08e87bfd.js"
+  }
+]);
